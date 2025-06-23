8475649a68ed157558b0d58179a073f74bf30133
danangdevs <danangdwisuta054@gmail.com>
2025-06-23 11:34:33 +0700

    fix login user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->session->userdata('email')) {
+            redirect('auth');
+        }
+        if ($this->session->userdata('role_id') != 2) {
+            redirect('auth');
+        }
+    }
     public function index()
     {
         $email = $this->session->userdata('email');
